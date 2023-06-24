63b028a041865989a2e35c2c25cf4873fcbc29e4
Kshitiz <kshitizs2008@gmail.com>
2023-06-25 01:33:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './Style.css'
+import Header from "./Header.jsx"
+import Meme from "./Meme.jsx"
+export default function App() {
+  return (
+      <div className="app">
+          <Header />
+          <Meme />
+      </div>
+  )
+}
